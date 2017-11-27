ea722114680ffb96977391e74ab1aeaaec8a41bf
andy <andy@ashall.co>
2017-11-27 16:55:12 +0000

    outlining how

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,10 @@
 module Main where
 
+-- What should be passed to game? new char to be added 
+type Game = (String, [String])
+
+-- Need to import a list of words 
+
 main :: IO ()
 main = do
   let letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
@@ -7,9 +12,18 @@ main = do
   if checkChar char letters
     then putStrLn "Valid"
     else putStrLn "Invalid"
-  
 
+game :: IO ()
+game = do
+
+-- Could be used to also check if a word is in a list
 checkChar :: String -> [String] -> Bool
 checkChar x l 
   | x `elem` l = True
   | otherwise = False
+
+-- addChar
+
+-- checkWord
+
+-- checkStartOfWord - need to check if current string is a valid start to a word
