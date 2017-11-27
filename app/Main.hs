b1b9544ab749cc7508cdcaa85e458ba83bd5ef19
andy <andy@ashall.co>
2017-11-27 16:45:17 +0000

    updated var and func names

--- app/Main.hs
+++ app/Main.hs
@@ -3,13 +3,13 @@ module Main where
 main :: IO ()
 main = do
   let letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-  val <- getLine
-  if check val letters
+  char <- getLine
+  if checkChar char letters
     then putStrLn "Valid"
     else putStrLn "Invalid"
   
 
-check :: String -> [String] ->Bool
-check x l 
+checkChar :: String -> [String] -> Bool
+checkChar x l 
   | x `elem` l = True
   | otherwise = False
